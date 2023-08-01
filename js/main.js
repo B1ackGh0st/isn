17db30ecb9d1a734a76ab870d9cfced6d87c1322
B1ackGh0st <b1ackghost2141@gmail.com>
2023-08-01 09:55:02 +0300

    first commit

--- js/main.js
+++ js/main.js
@@ -3,52 +3,6 @@
 	
 	jQuery(document).ready(function(){
 		
-		/* START MENU-JS */	
-			$('.nav a').on('click', function(e){
-				var anchor = $(this);
-				$('html, body').stop().animate({
-					scrollTop: $(anchor.attr('href')).offset().top - 50
-				}, 1500);
-				e.preventDefault();
-			});		
-
-	
-			$(window).scroll(function() {
-			  if ($(this).scrollTop() > 100) {
-				$('.menu-top').addClass('menu-shrink');
-			  } else {
-				$('.menu-top').removeClass('menu-shrink');
-			  }
-			});
-			
-			$(document).on('click','.navbar-collapse.in',function(e) {
-			if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
-				$(this).collapse('hide');
-			}
-			});				
-		/* END MENU-JS */
-		
-		/* START MOBILE-MENU  */
-			$('.main_menu').slicknav({
-				prependTo:".mobile-nav",
-			});
-		/* START MOBILE-MENU  */
-		 
-		/* START ISOTOP JS */
-			var $grid = $('.work_content_area').isotope({
-			  // options
-			});
-			// filter items on button click
-			$('.work_filter').on( 'click', 'li', function() {
-			  var filterValue = $(this).attr('data-filter');
-			  $grid.isotope({ filter: filterValue });
-			});
-			// filter items on button click
-			$('.work_filter').on( 'click', 'li', function() {
-				$(this).addClass('active').siblings().removeClass('active')
-			});
-		/* END ISOTOP JS */
-		
 		/* START LIGHTBOX */
 		
 			lightbox.option({
@@ -77,14 +31,4 @@
 		/* END COUNDOWN JS */
 		
 	});	
-	
-		/*PRELOADER JS*/
-			$(window).on('load', function() {  
-				$('.spinner').fadeOut();
-				$('.preloader').delay(350).fadeOut('slow'); 
-			}); 
-		/*END PRELOADER JS*/
-		
-		// Wow
-			new WOW().init();
 })(jQuery);
